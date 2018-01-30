a50e72d352c09be1948bd792f5d54c418a5cb9d8
Jiangxiaochen <1370035324@qq.com>
2018-01-30 09:43:31 +0800

    commit by cam

---
+++ csapp-study/ch3-assembly/test.c
@@ -0,0 +1,27 @@
+// //long a = 10;
+
+// void foo(void)
+// {
+// 	long b = 99;
+// 	//a = b;
+// }
+
+// void foo2(char * a, int b, short c, double d, float e, char f, long g, unsigned h){
+// 	a = "sss";
+// 	b = 12;
+// 	c = 13;
+// 	d = 14;
+// 	e = 15;
+// 	f = 16;
+// 	g = 17;
+// 	h = 18;
+// }
+
+void stack(int c)
+{
+	int a = c;
+	{
+		int b = 10;
+	}
+	int d = 11;
+}
