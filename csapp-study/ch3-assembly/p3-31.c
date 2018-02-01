5bd7253d843299f7ca76eaf1f80b81b2c6969eb8
Jiangxiaochen <1370035324@qq.com>
2018-02-01 15:06:29 +0800

    commit by cam

---
+++ csapp-study/ch3-assembly/p3-31.c
@@ -0,0 +1,18 @@
+switcher:
+	cmpq	$7,%rdi			//a>7
+	ja		.L2
+	jmp		*.L4(,%rdi,8)
+.L4:
+	.quad	.L3
+	.quad	.L2
+	.quad	.L5
+	.quad	.L2
+	.quad	.L6
+	.quad	.L7
+	.quad	.L2
+	.quad	.L5
+
+.L7:
+	xorq	$15,%rsi		//b^=15
+	movq	%rsi,%rdx		//c=b
+.
